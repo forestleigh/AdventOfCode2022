40bf75679ae7094d242ba93043bd88b5743f356d
forestleigh <theforestleigh@gmail.com>
2022-12-01 17:52:44 -0800

    solved AoC day 1 with python

--- DecOne/DecOne.js
+++ DecOne/DecOne.js
@@ -43,40 +43,41 @@ To begin, get your puzzle input.
 */
 
 // import Node's file reader
-const fs = require('fs');
+const fs = require("fs");
 // read contents of the file
-const data = fs.readFileSync('./DecOne.txt', 'UTF-8');
+const data = fs.readFileSync("./DecOne.txt", "UTF-8");
 
-  // split the contents by new line into array of substrings
-  // A carriage return (\r) + newline (\n) is best practice
-  const array = data.split(/\r?\n/);
+// split the contents by new line into array of substrings
+// A carriage return (\r) + newline (\n) is best practice
+const array = data.split(/\r?\n/);
 
-  const calCount = (arrayOfData) => {
-
+const calCount = (arrayOfData) => {
   // to parse string into elves and their calorie counts
   // initialize the elf cache, elf count, and max calorie count
   const elves = {};
   let currElf = 1;
   let maxCals = 0;
-  
-  arrayOfData.forEach(element => {
+
+  arrayOfData.forEach((element) => {
     // if the element is an eplty string, switch to the next elf
     if (!element) currElf++;
     if (elves[currElf]) {
-        elves[currElf] = elves[currElf] + Number(element);
+      elves[currElf] = elves[currElf] + Number(element);
     } else {
-        elves[currElf] = Number(element);
+      elves[currElf] = Number(element);
     }
-    maxCals = Math.max(maxCals,elves[currElf]);
+    maxCals = Math.max(maxCals, elves[currElf]);
   });
 
-  // use filter method in case more than one elf ties for the max calories brought 
-  const maxElf = Object.keys(elves).filter(
-     (key) => elves[key] === maxCals
-  );
+  // use filter method in case more than one elf ties for the max calories brought
+  const maxElf = Object.keys(elves).filter((key) => elves[key] === maxCals);
 
-  return ['Elf(s) with most calories: ', maxElf, 'max calorie amount: ', maxCals];
-
+  return [
+    "Elf(s) with most calories: ",
+    maxElf,
+    "max calorie amount: ",
+    maxCals,
+  ];
 };
 
 console.log(calCount(array));
@@ -94,36 +95,42 @@ Find the top three Elves carrying the most Calories. How many Calories are those
 */
 
 const calCount2 = (arrayOfData) => {
+  // to parse string into elves and their calorie counts
+  // initialize the elf cache, elf count, and max calorie count
+  const elves = {};
+  let currElf = 1;
 
-    // to parse string into elves and their calorie counts
-    // initialize the elf cache, elf count, and max calorie count
-    const elves = {};
-    let currElf = 1;
-    
-    arrayOfData.forEach(element => {
-      // if the element is an eplty string, switch to the next elf
-      if (!element) currElf++;
-      if (elves[currElf]) {
-          elves[currElf] = elves[currElf] + Number(element);
-      } else {
-          elves[currElf] = Number(element);
-      }
-    });
+  arrayOfData.forEach((element) => {
+    // if the element is an eplty string, switch to the next elf
+    if (!element) currElf++;
+    if (elves[currElf]) {
+      elves[currElf] = elves[currElf] + Number(element);
+    } else {
+      elves[currElf] = Number(element);
+    }
+  });
 
-    // sort the calories values in accending order
-    const calsArray = Object.values(elves).sort((a, b) => a - b);
-    // isolate the top 3 values
-    const topThree = calsArray.slice(-3);
-    // use reduce to add the top 3 values together
-    const maxCals = topThree.reduce((a, b)=> a + b);
-  
-    // use filter method to identify the top 3+ elves by their calorie amounts
-    const maxElves = Object.keys(elves).filter(
-        (key) => elves[key] === topThree[0] || elves[key] === topThree[1] || elves[key] === topThree[2]
-    );
-  
-    return ['Elves with the most calories: ', maxElves, 'max calorie amount: ', maxCals];
-  
-  };
-  
-  console.log(calCount2(array));
+  // sort the calories values in accending order
+  const calsArray = Object.values(elves).sort((a, b) => a - b);
+  // isolate the top 3 values
+  const topThree = calsArray.slice(-3);
+  // use reduce to add the top 3 values together
+  const maxCals = topThree.reduce((a, b) => a + b);
+
+  // use filter method to identify the top 3+ elves by their calorie amounts
+  const maxElves = Object.keys(elves).filter(
+    (key) =>
+      elves[key] === topThree[0] ||
+      elves[key] === topThree[1] ||
+      elves[key] === topThree[2]
+  );
+
+  return [
+    "Elves with the most calories: ",
+    maxElves,
+    "max calorie amount: ",
+    maxCals,
+  ];
+};
+
+console.log(calCount2(array));
---
+++ DecOne/DecOne.py
@@ -0,0 +1,30 @@
+# opening the file in a way that automatically closes it
+with open("/Users/fleigh/Projects/AdventofCode/DecOne/DecOne.txt", "r") as f:
+  # reading the data from the file
+  file_data = f.read()
+  # splitting the file data into lines
+  lines = file_data.splitlines()
+
+def maxCals(list):
+    # initialize elf counter and cache
+    curr = 1
+    elf_dict = {}
+
+    # iterate through the list
+    for i in list:
+        # when empty space, increment current elf
+        if i == '':
+            curr += 1
+        # otherwise, add to that elf's calorie sum
+        else:
+            elf_dict[curr] = elf_dict.get(curr, 0) + int(i)
+
+    # convert dict into list of tuples sorted by the calorie values
+    sorted_elves_by_cals = sorted(
+        elf_dict.items(), key=lambda x: x[1], reverse=True)
+
+    # return a list of the results 
+    return sorted_elves_by_cals[0:3]
+
+#check work
+print(maxCals(lines))
